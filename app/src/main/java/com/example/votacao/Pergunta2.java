9e1713972631b35cea57be4587a55c04483924ba
Igor Dias <igor@t2s.com.br>
2024-10-03 21:50:22 -0300

    Back-end start

--- app/src/main/java/com/example/votacao/Pergunta2.java
+++ app/src/main/java/com/example/votacao/Pergunta2.java
@@ -1,6 +1,7 @@
 package com.example.votacao;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Pergunta2 extends AppCompatActivity {
 
+    EditText etSeuVoto, etPrimeiroNome, etEstadoCivil, etIdade, etSexo, etTelefone, etData, etHora;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pergunta2);
 
+        etPrimeiroNome = findViewById(R.id.etPrimeiroNome);
+        etEstadoCivil = findViewById(R.id.etEstadoCivil);
+        etIdade = findViewById(R.id.etIdade);
+        etSexo = findViewById(R.id.etSexo);
+        etTelefone = findViewById(R.id.etTelefone);
+        etData = findViewById(R.id.etData);
+        etHora = findViewById(R.id.etHora);
+        etSeuVoto = findViewById(R.id.etSeuVoto);
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
